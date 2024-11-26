c0e8fb5e297604fdf3e5de1d2a1ce0b5d22eaa8e
tunchasan <hasantunc.dev@gmail.com>
2024-11-26 21:16:57 +0300

    Initial Commit

---
+++ The Ultimate Recipe Hub/The_Ultimate_Recipe_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Ultimate_Recipe_HubApp.swift
+//  The Ultimate Recipe Hub
+//
+//  Created by Personal on 26.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct The_Ultimate_Recipe_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
